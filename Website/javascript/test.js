94c349631e6f4228345370b2563af39fbf654bbf
wipark93 <wipark93@gmail.com>
2015-02-10 15:22:03 -0600

    Updated Website
    CSS needs to be adjusted

--- Website/javascript/test.js
+++
@@ -1,6 +0,0 @@
-$(function() { //When the document loads
-  $("#scrollTop").bind("click", function() {
-    $(window).scrollTop($("#scroll").offset().top); //scroll to div with container as ID.
-    return false; //Prevent Default and event bubbling.
-  });
-});
