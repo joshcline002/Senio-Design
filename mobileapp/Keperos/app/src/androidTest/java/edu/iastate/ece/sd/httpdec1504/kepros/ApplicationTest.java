7bed5262e8638831cfa1e1b43d104d07f75a3586
joshcline002 <joshcline002@gmail.com>
2015-02-10 19:39:15 -0600

    Start of android app

---
+++ mobileapp/Keperos/app/src/androidTest/java/edu/iastate/ece/sd/httpdec1504/kepros/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.iastate.ece.sd.httpdec1504.kepros;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
