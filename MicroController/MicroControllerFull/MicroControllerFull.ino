bd2786b9cf10ee1053ae3d7b0e979c682925d9dc
Joshua C Cline <joshcline002@gmail.com>
2015-04-13 14:51:46 -0500

    Added Bluetooth
    Added data transmission over bluetooth

--- MicroController/MicroControllerFull/MicroControllerFull.ino
+++ MicroController/MicroControllerFull/MicroControllerFull.ino
@@ -1,26 +1,22 @@
-//Gotten from Muscle-Sensor-V3 github page
-
-// reads analog input from the five inputs from your arduino board 
-// and sends it out via serial
-
 // variables for input pins and
 int num_analog_in = 1;
 int analogInput[1];
 // variable to store the value 
 int value[1]; 
- 
+
 void setup()
 {
   // declaration of pin modes
   for(int i=0;i<num_analog_in;i++)
   {
-    analogInput[i] = i+1;
+    analogInput[i] = 17+1;
     value[i] = 0;     
     pinMode(analogInput[i], INPUT);    
   }
   
   // begin sending over serial port
   Serial.begin(9600);
+  Serial1.begin(9600);
 }
 
 void loop()
@@ -35,8 +31,11 @@ void loop()
   for(int i=0;i<num_analog_in;i++)
   {
     Serial.println(10000 + i + 1); //prefix
+    Serial1.println(1000 + i + 1);
     Serial.println(value[i]);
+    Serial1.println(value[i]);
     Serial.println(10010); //end signal
+    Serial1.println(10010); //end signal
   }
   // wait for a bit to not overload the port
   delay(10);
