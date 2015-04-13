ebdf0c9fd34aab884b58bf1a59b0aa670f8249f7
Joshua C Cline <joshcline002@gmail.com>
2015-04-13 17:21:47 -0500

    Revert "Oops"
    This reverts commit 4f9fbf691a6d47956a245ebccee4981cb6b43661.

--- MicroController/MicroControllerFull/MicroControllerFull.ino
+++ MicroController/MicroControllerFull/MicroControllerFull.ino
@@ -9,20 +9,14 @@ void setup()
   // declaration of pin modes
   for(int i=0;i<num_analog_in;i++)
   {
-    analogInput[i] = 17+1+i;
+    analogInput[i] = 17+1;
     value[i] = 0;     
-    pinMode(18, INPUT);    
+    pinMode(analogInput[i], INPUT);    
   }
-  //Serial1.begin(115200);  // The Bluetooth Mate defaults to 115200bps
-  //Serial1.print("$");  // Print three times individually
-  //Serial1.print("$");
-  //Serial1.print("$");  // Enter command mode
-  //delay(100);  // Short delay, wait for the Mate to send back CMD
-  //Serial1.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
-  // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
+  
   // begin sending over serial port
   Serial.begin(9600);
-  //Serial1.begin(9600);
+  Serial1.begin(9600);
 }
 
 void loop()
@@ -36,16 +30,14 @@ void loop()
   // print out value over the serial port
   for(int i=0;i<num_analog_in;i++)
   {
-    //Serial.println(10000 + i + 1); //prefix
-    //Serial1.println(1000 + i + 1);
-    
-    Serial.println(analogRead(19));
-    //Serial1.println(value[i]);
-    
-    //Serial.println(10010); //end signal
-    //Serial1.println(10010); //end signal
+    Serial.println(10000 + i + 1); //prefix
+    Serial1.println(1000 + i + 1);
+    Serial.println(value[i]);
+    Serial1.println(value[i]);
+    Serial.println(10010); //end signal
+    Serial1.println(10010); //end signal
   }
   // wait for a bit to not overload the port
-  delay(100);
+  delay(10);
 }
 
