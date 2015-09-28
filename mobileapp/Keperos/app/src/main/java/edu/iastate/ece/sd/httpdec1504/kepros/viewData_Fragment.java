1690bfd99f7c1617891ea5d40635ef88bb1ddfe3
Joshua C Cline <joshcline002@gmail.com>
2015-09-28 14:36:41 -0500

    ViewData Speed and clean up
    Attempt to speed up and clean up the view data fragment.

--- mobileapp/Keperos/app/src/main/java/edu/iastate/ece/sd/httpdec1504/kepros/viewData_Fragment.java
+++ mobileapp/Keperos/app/src/main/java/edu/iastate/ece/sd/httpdec1504/kepros/viewData_Fragment.java
@@ -1,8 +1,6 @@
 package edu.iastate.ece.sd.httpdec1504.kepros;
 
-import android.app.Fragment;
 import android.os.Bundle;
-import android.os.HandlerThread;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -14,22 +12,16 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.Intent;
 import android.os.Handler;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -64,8 +56,7 @@ public class viewData_Fragment extends android.support.v4.app.Fragment {
     Handler bluetoothHandler = new Handler() {
         @Override
         public void handleMessage(android.os.Message msg) {
-                String readMessage = (String) msg.obj.toString();
-                recDataString.append(readMessage);
+                recDataString.append(msg.obj.toString());
                  mylist.add(String.valueOf(recDataString));
                  recDataString.delete(0, recDataString.length());                    //clear all string data
                 }
@@ -92,44 +83,44 @@ public class viewData_Fragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.viewdata_layout, container, false);
+        CheckAdapter = new ArrayAdapter<String>(rootview.getContext(), android.R.layout.simple_list_item_1);
         return rootview;
-
     }
 
     @Override
     public void onResume() {
         super.onResume();  // Always call the superclass method first
-
-        CheckAdapter = new ArrayAdapter<String>(rootview.getContext(), android.R.layout.simple_list_item_1);
         list = (ListView) rootview.findViewById(R.id.datalist);
         list.setAdapter(CheckAdapter);
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
-        MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
-// If there are paired devices
-        if (pairedDevices.size() > 0) {
-            // Loop through paired devices
-            for (BluetoothDevice device : pairedDevices) {
-                if(device.getName().compareTo("Posture") == 0) {
-                    myBTDevice=device;
-                    CheckAdapter.add(device.getName());
+        init.start();
+        bluetoothHandler.postDelayed(stringToList, 10);
+    }
+
+    private Thread init = new Thread(){
+        @Override
+        public void start() {
+            btAdapter = BluetoothAdapter.getDefaultAdapter();
+            MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+            Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        // If there are paired devices
+            if (pairedDevices.size() > 0) {
+                // Loop through paired devices
+                for (BluetoothDevice device : pairedDevices) {
+                    if(device.getName().compareTo("Posture") == 0) {
+                        myBTDevice=device;
+                    }
                 }
             }
+            connectThread.start();
         }
-        CheckAdapter.add(MY_UUID.toString());
-
-        //mHandler.postDelayed(stringToList, 10);
-        bluetoothHandler.postDelayed(stringToList, 10);
-        ConnectThread();
-        run();
-    }
+    };
 
     private Thread thread = new Thread() {
         @Override
         public void run() {
             try {
                 while (true) {
-                    sleep(1000);
+                    sleep(10);
                     manageConnectedSocket();;
                 }
             } catch (InterruptedException e) {
@@ -138,22 +129,27 @@ public class viewData_Fragment extends android.support.v4.app.Fragment {
         }
     };
 
+    private Thread connectThread = new Thread() {
+        @Override
+        public void start() {
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
+            BluetoothSocket tmp = null;
 
-    public void ConnectThread() {
-        // Use a temporary object that is later assigned to mmSocket,
-        // because mmSocket is final
-        BluetoothSocket tmp = null;
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // MY_UUID is the app's UUID string, also used by the server code
+                tmp = myBTDevice.createInsecureRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+            }
+            btSocket = tmp;
+            run.start();
+        }
+    };
 
-        // Get a BluetoothSocket to connect with the given BluetoothDevice
-        try {
-            // MY_UUID is the app's UUID string, also used by the server code
-            tmp = myBTDevice.createInsecureRfcommSocketToServiceRecord(MY_UUID);
-            mylist.add("tmp = myBTDevice.createRfcommSocketToServiceRecord(MY_UUID);");
-        } catch (IOException e) { }
-        btSocket = tmp;
-    }
-
-    public void run() {
+    private Thread run = new Thread(){
+    @Override
+    public void start() {
         // Cancel discovery because it will slow down the connection
         btAdapter.cancelDiscovery();
 
@@ -161,19 +157,19 @@ public class viewData_Fragment extends android.support.v4.app.Fragment {
             // Connect the device through the socket. This will block
             // until it succeeds or throws an exception
             btSocket.connect();
-            mylist.add("btSocket.connect();");
             Log.d("BLUETOOTHSOCKET", "Bluetooth Socket Connected!");
             thread.start();
         } catch (IOException connectException) {
             // Unable to connect; close the socket and get out
             try {
                 btSocket.close();
-                mylist.add("Closed");
-            } catch (IOException closeException) { }
+                Log.d("BLUETOOTHSOCKET", "Bluetooth Socket Closed!");
+            } catch (IOException closeException) {
+            }
             return;
         }
-
     }
+    };
 
     /** Will cancel an in-progress connection, and close the socket */
     public void cancel() {
@@ -227,12 +223,4 @@ public class viewData_Fragment extends android.support.v4.app.Fragment {
             bluetoothHandler.postDelayed(stringToList, 10);
         }
     };
-
-    private Runnable stop = new Runnable() {
-        public void run() {
-           cancel();
-        }
-    };
-
-
 }
